81dc8c5e8d4b135d7e45899d4b707db367b60462
Guilherme Luis Faustino <guilhermel.faustino@gmail.com>
2020-05-10 22:54:16 -0300

    add demanda 04

--- leandro/demanda03/js/custom/semicircle.js
+++ leandro/demanda03/js/custom/semicircle.js
@@ -1,43 +1,35 @@
 am4core.ready(function () {
   // Themes begin
   am4core.useTheme(am4themes_animated);
+  
   // Themes end
-
   var chart = am4core.create("semiCircleChartdiv", am4charts.PieChart);
-
-  chart.properties.dy = -10;
   chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
   chart.responsive.enabled = true;
+
+  chart.language.locale = am4lang_pt_BR;
+  chart.dateFormatter.language = new am4core.Language();
+  chart.dateFormatter.language.locale = am4lang_pt_BR;
+
   chart.data = DATAEXCEL;
+
 
   chart.radius = am4core.percent(70);
   chart.innerRadius = am4core.percent(40);
   chart.startAngle = 180;
   chart.endAngle = 360;
 
+
   var series = chart.series.push(new am4charts.PieSeries());
   series.maxLevels = 1;
   series.dataFields.value = "SomatorioM";
   series.dataFields.category = "NomeCluster";
-
   series.dataFields.categoryX = "Interpretacao";
   series.slices.template.propertyFields.fill = "color";
-
-  series.slices.template.tooltipHTML =
-    "<p>{Interpretacao} | Qt. cliente {QtdClientes}; Tícket Médio: {MediaM}; Volume de Vendas: {SomatorioM} </>";
-
-  series.slices.template.events.on(
-    "hit",
-    function (ev) {
-      //object json
-      console.log(ev.target.dataItem.dataContext);
-      console.warn(
-        `====Clicado em ${ev.target.dataItem.dataContext.NomeCluster}`
-      );
-    },
-    this
-  );
-
+  
+  series.slices.template.tooltipText =
+      "[bold]Cluster {NomeCluster} - {Interpretacao}[\]\nGastos: R$ {SomatorioM.formatNumber('#,###.00')}\nTicket Médio: R$ {MediaM.formatNumber('#,###.00')}\n{QtdClientes} clientes\n{value.percent.formatNumber('#.0')}% dos Gastos";
+ 
   series.slices.template.cornerRadius = 10;
   series.slices.template.innerCornerRadius = 7;
   series.slices.template.draggable = false;
@@ -47,39 +39,47 @@ am4core.ready(function () {
   series.hiddenState.properties.startAngle = 90;
   series.hiddenState.properties.endAngle = 90;
 
-  /* series.labels.template.relativeRotation = 90;
-series.ticks.template.disabled = true;
-series.alignLabels = false;
-series.labels.template.radius = am4core.percent(-40);
-series.labels.template.fill = am4core.color("black");
-*/
+
   series.labels.template.paddingTop = 0;
   series.labels.template.paddingBottom = 0;
-  series.labels.template.fontSize = 10;
+  series.labels.template.fontSize = 14;
 
   series.labels.template.text =
-    "{NomeCluster} | {value.percent.formatNumber('#.0')}%";
+  "{NomeCluster} | {value.formatNumber('#,###.00')}%";
 
-  //https://www.amcharts.com/demos/variable-radius-nested-donut-chart/
-  var label = chart.seriesContainer.createChild(am4core.Label);
+  //Remove a descriçaõ dos Labels da série do gráfico
+  series.labels.template.disabled = true;
+
+/*  series.ticks.template.adapter.add("hidden", hideSmall);
+  series.labels.template.adapter.add("hidden", hideSmall);
+  function hideSmall(hidden, target) {
+      return target.dataItem.values.value.percent < 100 ? true : false;
+  }*/
+
+  var label = series.createChild(am4core.Label);
   label.textAlign = "middle";
   label.horizontalCenter = "middle";
   label.verticalCenter = "middle";
-
+  label.y = -10; // -70  // font-size: 30px
   label.adapter.add("text", function (text, target) {
     return (
-      "[font-size:18px]total[/]:\n[bold font-size:22px]" +
-      series.dataItem.values.value.sum +
-      "[/]"
-    );
+      "Total[/]:\n[bold font-size:24px]" + new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(series.dataItem.values.value.sum) + "[/]"
+      );
   });
+
 
   var legend = new am4charts.Legend();
   legend.parent = chart.chartContainer;
   legend.align = "bottom";
   legend.valign = "bottom";
+  legend.marginTop = "200px";
+
+  /*var legend = new am4charts.Legend();
+  legend.parent = chart.chartContainer;
+  legend.align = "bottom";
+  legend.valign = "bottom";
   legend.marginTop = "5px";
-  legend.fontSize = 10;
+  legend.fontSize = 10;*/
 
   series.events.on("ready", function (ev) {
     var legenddata = [];
@@ -121,10 +121,10 @@ series.labels.template.fill = am4core.color("black");
 
     teste2 = Object.keys(teste).map((el) => {
       let valueSlice = sumValue[el].reduce((a, b) => a + b, 0).toFixed(2);
-      teste[el].name = teste[el].name + " | " + valueSlice + "%";
+      //teste[el].name = teste[el].name + " | " + valueSlice + "%";
       let dataSequence = { sequence: sequenceNumber[el] };
       return { ...teste[el], ...dataSequence };
-    });
+  });
 
     legend.data = teste2;
 
@@ -139,41 +139,49 @@ series.labels.template.fill = am4core.color("black");
 
   chart.responsive.rules.push({
     relevant: function (target) {
-      // if (target.pixelWidth <= 641) {
-
-      if (true) {
-        console.log("menor", target.pixelWidth);
-        return true;
-      }
-      return false;
+        if (target.pixelWidth <= 600) {
+            return true;
+        }
+        return false;
     },
     state: function (target, stateId) {
-      if (target instanceof am4charts.PieChart) {
-        var state = target.states.create(stateId);
-        console.warn("Entrei aqui");
-        state.properties.y = -70;
+        if (target instanceof am4charts.PieChart) {
+            var state = target.states.create(stateId);
 
-        return state;
-      } else if (target instanceof am4charts.Legend) {
-        var state = target.states.create(stateId);
-        state.sprite.fontSize = 8;
-        state.sprite.dy = 50;
-        return state;
-      }
+            state.properties.y = -110;
 
-      if (target.text == "Total {values.value.sum}") {
-        var state = target.states.create(stateId);
+            return state;
+        } else if (target instanceof am4charts.Legend) {
+            var state = target.states.create(stateId);
+            state.sprite.fontSize = 8;
+            state.sprite.dy = -20;
+            return state;
+        }
 
-        state.sprite.fontSize = 21;
-        state.sprite.y = 15;
-        state.sprite.x = window.innerWidth / 2 - 146.7 + 20;
-        state.sprite.textAlign = "center";
-        return state;
-      }
-      return null;
+        if (target.text) {
+          if(target.text.indexOf("Total") != -1){
+            var state = target.states.create(stateId);
+
+            if(myapp_config.isMobile){
+              state.sprite.y = 15;
+              return state;
+            } else {
+              state.sprite.y = -10;
+            }
+            
+          }
+            
+        }
+        return null;
     },
   });
+
+
 }); // end am4core.ready()
+
+function detectMob() {
+  return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
+}
 
 function legenda() {
   let legenda = document.querySelectorAll(
