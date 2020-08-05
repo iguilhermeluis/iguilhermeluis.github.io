acf2844edd9c412140cfe365beee3da0ce95529c
Guilherme Luis Faustino <guilhermel.faustino@gmail.com>
2020-08-05 02:52:04 -0300

    demanda fix

--- leandro/demanda_20-07-27_legenda/js/util/toggleCheck.js
+++ leandro/demanda_20-07-27_legenda/js/util/toggleCheck.js
@@ -1,23 +1,42 @@
-function toggleCheck(seriesChart, isGlobal, filter, valueSearch, checkId, uncheckId, colorSearch) {
+function toggleCheck(
+  seriesChart,
+  isGlobal,
+  filter,
+  valueSearch,
+  checkId,
+  uncheckId,
+  colorSearch
+) {
   document.getElementById(checkId).addEventListener("click", (e) => {
     seriesChart.map((el) => {
       el.dataItems.values.map((value) => {
-    
-        if(isGlobal){
+        if (isGlobal) {
           value.show();
-          document.querySelectorAll(`g[role="switch"]  g[fillBackup="${value.dataContext['CorFundoR']}"]`).forEach(el=> el.setAttribute('fill', el.getAttribute('fillBackup') ))
-          document.querySelectorAll(`g[role="switch"]  g[fillBackup="${value.dataContext['CorFundoFM']}"]`).forEach(el=> el.setAttribute('fill', el.getAttribute('fillBackup') ))
+          document
+            .querySelectorAll(
+              `g[role="switch"]  g[fillBackup="${value.dataContext["CorFundoR"]}"]`
+            )
+            .forEach((el) =>
+              el.setAttribute("fill", el.getAttribute("fillBackup"))
+            );
+          document
+            .querySelectorAll(
+              `g[role="switch"]  g[fillBackup="${value.dataContext["CorFundoFM"]}"]`
+            )
+            .forEach((el) =>
+              el.setAttribute("fill", el.getAttribute("fillBackup"))
+            );
+          return true;
         }
 
-        if(value.dataContext[filter] == valueSearch) {
-          value.show();
-
-          
-          document.querySelectorAll(`g[role="switch"]  g[fillBackup="${value.dataContext['CorFundoR']}"]`).forEach(el=> el.setAttribute('fill', el.getAttribute('fillBackup') ))
-          document.querySelectorAll(`g[role="switch"]  g[fillBackup="${value.dataContext['CorFundoFM']}"]`).forEach(el=> el.setAttribute('fill', el.getAttribute('fillBackup') ))
-          
-        }
-        
+        value.show();
+        document
+          .querySelectorAll(
+            `g[role="switch"]  g[fillBackup="${value.dataContext[colorSearch]}"]`
+          )
+          .forEach((el) =>
+            el.setAttribute("fill", el.getAttribute("fillBackup"))
+          );
       });
     });
     document.getElementById(uncheckId).checked = false;
@@ -26,34 +45,39 @@ function toggleCheck(seriesChart, isGlobal, filter, valueSearch, checkId, unchec
   document.getElementById(uncheckId).addEventListener("click", (e) => {
     seriesChart.map((el) => {
       el.dataItems.values.map((value) => {
-      
-        if(isGlobal){
+        if (isGlobal) {
           value.hide();
 
-          document.querySelectorAll(`g[role="switch"]  g[fill="${value.dataContext['CorFundoR']}"]`).forEach(el=>
-            {
-              el.setAttribute('fillBackup', el.getAttribute('fill'));
-              el.setAttribute('fill', '#999999');
-            }
-          )
-          
-          document.querySelectorAll(`g[role="switch"]  g[fill="${value.dataContext['CorFundoFM']}"]`).forEach(el=>
-            {
-              el.setAttribute('fillBackup', el.getAttribute('fill'));
-              el.setAttribute('fill', '#999999');
-            }
-          )
-        } 
+          document
+            .querySelectorAll(
+              `g[role="switch"]  g[fill="${value.dataContext["CorFundoR"]}"]`
+            )
+            .forEach((el) => {
+              el.setAttribute("fillBackup", el.getAttribute("fill"));
+              el.setAttribute("fill", "#999999");
+            });
 
-        if(value.dataContext[filter] == valueSearch){
-          value.hide();
-          /*document.querySelectorAll(`g[role="switch"]  g[fill="${value.dataContext[colorSearch]}"]`).forEach(el=>
-            {
-              el.setAttribute('fillBackup', el.getAttribute('fill'));
-              el.setAttribute('fill', '#999999');
-            }
-          )*/
+          document
+            .querySelectorAll(
+              `g[role="switch"]  g[fill="${value.dataContext["CorFundoFM"]}"]`
+            )
+            .forEach((el) => {
+              el.setAttribute("fillBackup", el.getAttribute("fill"));
+              el.setAttribute("fill", "#999999");
+            });
+
+          return true;
         }
+
+        value.hide();
+        document
+          .querySelectorAll(
+            `g[role="switch"]  g[fill="${value.dataContext[colorSearch]}"]`
+          )
+          .forEach((el) => {
+            el.setAttribute("fillBackup", el.getAttribute("fill"));
+            el.setAttribute("fill", "#999999");
+          });
       });
     });
     document.getElementById(checkId).checked = false;
